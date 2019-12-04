c9e9634ffe8bd56a57cc7f25854e29d27e242707
Anton Mayatskiy <worknekki@gmail.com>
2019-12-04 17:37:51 +1000

    Top-bar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {AngularSvgIconModule} from 'angular-svg-icon';
 import {HttpClientModule} from '@angular/common/http';
+import { TopToolbarComponent } from './view/top-toolbar/top-toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopToolbarComponent
   ],
   imports: [
     BrowserModule,
